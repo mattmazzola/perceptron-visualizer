1a8c0ddab99fcafd0f61984b6320efa9bdd1b183
Matt Mazzola <mattmazzola@live.com>
2016-07-19 00:04:10 -0700

    Refactor into class to make it more like re-usable chart pattern.

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import * as perceptron from '@mattmazzola/perceptron';
+
 import {
   selectAll,
   select,
@@ -23,34 +24,7 @@ import {
   drag
 } from "d3-drag";
 
-const data = [1,2,3,4];
-const dataExtents = extent(data);
-const chart = {
-  width: 400,
-  height: 400,
-  paddingTop: 30,
-  paddingRight: 30,
-  paddingBottom: 30,
-  paddingLeft: 30
-};
-console.log('dataExtents', dataExtents);
-
-const scaleX = scaleLinear()
-  .domain([-50, 50])
-  .range([chart.paddingLeft, chart.width - chart.paddingRight]);
-
-const scaleY = scaleLinear()
-  .domain([-50, 50])
-  .range([chart.height - chart.paddingBottom, chart.paddingTop]);
-
-const axisX = axisBottom(scaleX);
-const axisY = axisLeft(scaleY);
-
-interface IEntity {
-  id: number;
-}
-
-interface ILine extends IEntity {
+export interface ILine {
   x1: number;
   y1: number;
   x2: number;
@@ -59,165 +33,209 @@ interface ILine extends IEntity {
   strokeWidth: number;
 }
 
-interface ICircle extends IEntity {
+export interface ICircle {
   x: number;
   y: number;
   r: number;
   color: string;
 }
-const circles: ICircle[] = [];
-const container =  select(".visual-container");
 
-let pointMode = false;
-const modeButton = select("button.mode");
-modeButton
-  .on('click', modeButtonClicked);
+export class Chart {
+  // Defaults
+  width: number = 400;
+  height: number = 400;
+  paddingTop: number = 30;
+  paddingRight: number = 30;
+  paddingBottom: number = 30;
+  paddingLeft: number = 30;
 
-function modeButtonClicked() {
-  pointMode = !pointMode;
-  console.log('mode button clicked', pointMode);
-}
+  xScale: any; //Function;
+  yScale: any; //Function;
 
-const svg = container
-  .append("svg")
-  .attr("width", '100%')
-  .attr("height", '100%');
-
-const axisXgroup = svg
-  .append('g')
-  .attr('transform', `translate(0, ${(chart.height)/2})`)
-  .classed('axis', true)
-  .classed('axis--x', true)
-  .call(axisX);
-
-const axisYGroup = svg
-  .append('g')
-  .attr('transform', `translate(${chart.width/2}, 0)`)
-  .classed('axis', true)
-  .classed('axis--y', true)
-  .call(axisY);
-
-function update() {
-  svg
-    .selectAll('circle')
-    .data(circles)
-    .enter()
-      .append("circle")
-      .attr("cx", (d: ICircle) => scaleX(d.x))
-      .attr("cy", (d: ICircle) => scaleY(d.y))
-      .attr("r", (d: ICircle) => d.r)
-      .style("fill", (d: ICircle) => d.color);
-}
-
-update();
-
-svg.on('click', function (event: any) {
-  if(pointMode) {
-    const [x,y] = mouse(this);
-    addPoint(x,y);
-  }
-});
-
-function addPoint(x: number, y: number) {
-  const scaledCoordinates = {
-    x: Math.round( scaleX.invert(x)),  // Takes the pixel number to convert to number
-    y: Math.round( scaleY.invert(y))
+  circles: ICircle[] = [];
+  containgElement: any;
+  dragStartPoint: any = {
+    id: 100,
+    x: 0,
+    y: 0,
+    scaledX: 0,
+    scaledY: 0
   };
-
-  const newCircle: ICircle = {
-    id: Math.floor(Math.random() * 100),
-    x: scaledCoordinates.x,
-    y: scaledCoordinates.y,
-    r: 5,
-    color: 'red'
+  dragEndPoint: any = {
+    x: 0,
+    y: 0,
+    scaledX: 0,
+    scaledY: 0
   };
+  dragBehavior: any;
+  trainingLine: any = {
+    slope: -1,
+    offset: 0
+  };
+  svg: any;
 
-  circles.push(newCircle);   // Push data to our array
+  mode: boolean = true;
 
-  update();
-}
+  constructor(selector: string, domain: [number, number], data: any[]) {
+    this.containgElement = select(selector);
 
-const dragBehavior = drag()
-  .container(function() { return this; })
-  .subject(function() { var p = [event.x, event.y]; return [p, p]; })
-  .on("start", dragstarted)
-  .on("drag", dragmove)
-  .on("end", dragended);
+    // Inject svg
+    this.svg = this.containgElement
+      .append("svg")
+      .attr("width", '100%')
+      .attr("height", '100%');
 
-svg
-    .call(dragBehavior);
+    // Create scales mapping domain coordinates to svg coordinates
+    this.xScale = scaleLinear()
+      .domain(domain)
+      .range([this.paddingLeft, this.width - this.paddingRight]);
 
-let dragStartPoint = {
-  id: 10,
-  x: 0,
-  y: 0,
-  scaledX: 0,
-  scaledY: 0
-};
+    this.yScale = scaleLinear()
+      .domain(domain)
+      .range([this.height - this.paddingBottom, this.paddingTop]);
 
-function dragstarted() {
-  if (!pointMode) {
-    const [x,y] = [event.x, event.y];
-    dragStartPoint.id += 1;
-    dragStartPoint.x = x;
-    dragStartPoint.scaledX = Math.round(scaleX.invert(x));
-    dragStartPoint.y = y;
-    dragStartPoint.scaledY = Math.round(scaleY.invert(y)); 
+    // Create axises
+    const xAxis = axisBottom(this.xScale);
+    const yAxis = axisLeft(this.yScale);
+
+    // Draw Axises
+    const xAxisGroup = this.svg
+      .append('g')
+      .attr('transform', `translate(0, ${(this.height)/2})`)
+      .classed('axis', true)
+      .classed('axis--x', true)
+      .classed('noselect', true)
+      .call(xAxis);
+
+    const yAxisGroup = this.svg
+      .append('g')
+      .attr('transform', `translate(${this.width/2}, 0)`)
+      .classed('axis', true)
+      .classed('axis--y', true)
+      .classed('noselect', true)
+      .call(yAxis);
+
+    // Register Events
+    this.svg.on('click', (() => {
+      const that = this;
+      return function () {
+        const [x,y] = mouse(this);
+        console.log('new svg clicked');
+        if (that.mode) {
+          that.addPoint(x,y);
+        }
+      }
+    })());
+
+    this.dragBehavior = drag()
+      .container(function() { return this; })
+      .subject(function() { var p = [event.x, event.y]; return [p, p]; })
+      .on("start", () => this.dragStarted())
+      .on("drag", () => this.dragMove())
+      .on("end", () => this.dragEnded());
   }
-}
 
-function dragmove() {
-  if (!pointMode) {
-    const [x,y] = [event.x, event.y];
-    const dragMovePoint = {
-      x,
-      y,
-      scaledX: Math.round(scaleX.invert(x)),
-      scaledY: Math.round(scaleY.invert(y))
-    };
+  reset() {
+    this.circles.length = 0;
+    this.update();
 
-    let run: number;
-    let rise: number;
-    let slope: number;
-    let offset: number;
-    let minX: number = 0;
-    let minY: number;
-    let maxX: number = 400;
-    let maxY: number;
+    this.svg
+      .selectAll('line')
+      .remove('line')
 
-    run = (dragMovePoint.x - dragStartPoint.x);
-    rise = (dragMovePoint.y - dragStartPoint.y);
-    
-    if(run === 0) {
-      slope = Infinity;
-      offset = 0;
-      minY = 0;
-      minX = dragMovePoint.x,
-      maxY = 400;
-      maxX = dragMovePoint.x
+  }
+
+  toggleMode() {
+    this.mode = !this.mode;
+
+    if (!this.mode) {
+      this.svg
+        .call(this.dragBehavior);
     }
     else {
-      slope = rise/run;
-      offset = dragMovePoint.y - slope * dragMovePoint.x;
+      this.svg.on('.drag', null);
+    }
+  }
 
-      minY = slope * (0) + offset;
-      maxY = slope * 400 + offset;
+  private addPoint(x: number, y: number) {
+    const scaledCoordinates = {
+      x: Math.round(this.xScale.invert(x)),
+      y: Math.round(this.yScale.invert(y))
+    };
 
-      if (slope === 0) {
-        minX = 0;
-        maxX = 400;
+    const newCircle: ICircle = {
+      x: scaledCoordinates.x,
+      y: scaledCoordinates.y,
+      r: 5,
+      color: 'blue'
+    };
+
+    this.circles.push(newCircle);
+    this.update();
+  }
+
+  private dragStarted() {
+    if (!this.mode) {
+      const [x,y] = [event.x, event.y];
+      this.dragStartPoint.x = x;
+      this.dragStartPoint.scaledX = Math.round(this.xScale.invert(x));
+      this.dragStartPoint.y = y;
+      this.dragStartPoint.scaledY = Math.round(this.yScale.invert(y));
+    }
+  }
+
+  private dragMove() {
+    if (!this.mode) {
+      const [x,y] = [event.x, event.y];
+      const dragMovePoint = {
+        x,
+        y,
+        scaledX: Math.round(this.xScale.invert(x)),
+        scaledY: Math.round(this.yScale.invert(y))
+      };
+
+      let run: number;
+      let rise: number;
+      let slope: number;
+      let offset: number;
+      let minX: number = 0;
+      let minY: number;
+      let maxX: number = 400;
+      let maxY: number;
+
+      run = (dragMovePoint.x - this.dragStartPoint.x);
+      rise = (dragMovePoint.y - this.dragStartPoint.y);
+      
+      if(run === 0) {
+        slope = Infinity;
+        offset = 0;
+        minY = 0;
+        minX = dragMovePoint.x,
+        maxY = 400;
+        maxX = dragMovePoint.x
       }
       else {
-        minX = (minY - offset)/slope;
-        maxX = (maxY - offset)/slope;
-      }
-    }
+        slope = rise/run;
+        offset = dragMovePoint.y - slope * dragMovePoint.x;
 
-    const selection = svg
+        minY = slope * (0) + offset;
+        maxY = slope * 400 + offset;
+
+        if (slope === 0) {
+          minX = 0;
+          maxX = 400;
+        }
+        else {
+          minX = (minY - offset)/slope;
+          maxX = (maxY - offset)/slope;
+        }
+      }
+
+      const selection = this.svg
         .selectAll('line')
         .data([
           {
-            id: -dragStartPoint.id,
+            id: -this.dragStartPoint.id,
             x1: minX,
             y1: minY,
             x2: maxX,
@@ -226,9 +244,9 @@ function dragmove() {
             strokeWidth: 2
           },
           {
-            id: dragStartPoint.id,
-            x1: dragStartPoint.x,
-            y1: dragStartPoint.y,
+            id: this.dragStartPoint.id,
+            x1: this.dragStartPoint.x,
+            y1: this.dragStartPoint.y,
             x2: dragMovePoint.x,
             y2: dragMovePoint.y,
             stroke: 'red',
@@ -236,7 +254,7 @@ function dragmove() {
           },
         ], (d: any) => d.id);
 
-    selection
+      selection
         .enter()
           .append('line')
           .attr('x1', (d: ILine) => d.x1)
@@ -248,68 +266,68 @@ function dragmove() {
           .classed('division', true)
         ;
 
+      selection
+        .attr('x1', (d: ILine) => d.x1)
+        .attr('y1', (d: ILine) => d.y1)
+        .attr('x2', (d: ILine) => d.x2)
+        .attr('y2', (d: ILine) => d.y2)
+        ;
+
+      selection
+        .exit()
+          .remove('line')
+          ;
+    }
+  }
+
+  private dragEnded() {
+    if (!this.mode) {
+      const [x,y] = [event.x, event.y];
+      this.dragEndPoint.x = x;
+      this.dragEndPoint.scaledX = Math.round(this.xScale.invert(x));
+      this.dragEndPoint.y = y;
+      this.dragEndPoint.scaledY = Math.round(this.yScale.invert(y));
+    }
+  }
+
+  private update() {
+    const selection = this.svg
+      .selectAll('circle')
+      .data(this.circles);
+
     selection
-      .attr('x1', (d: ILine) => d.x1)
-      .attr('y1', (d: ILine) => d.y1)
-      .attr('x2', (d: ILine) => d.x2)
-      .attr('y2', (d: ILine) => d.y2)
-      ;
+      .enter()
+        .append("circle")
+        .attr("cx", (d: ICircle) => this.xScale(d.x))
+        .attr("cy", (d: ICircle) => this.yScale(d.y))
+        .attr("r", (d: ICircle) => d.r)
+        .style("fill", (d: ICircle) => d.color);
+
+    selection
+      .attr("cx", (d: ICircle) => this.xScale(d.x))
+      .attr("cy", (d: ICircle) => this.yScale(d.y));
 
     selection
       .exit()
-        .remove('line')
-        ;
+        .remove('circle');
+  }
+
+  private updateTrainingLine(slope: number, offset: number) {
+    const selection = this.svg
+      .selectAll('line.train')
+      .data()
   }
 }
 
-function dragended() {
-  if (!pointMode) {
-    const [x,y] = [event.x, event.y];
-    const scaledCoordinates = {
-      x: Math.round(scaleX.invert(x)),
-      y: Math.round(scaleY.invert(y))
-    };
-  }
-}
 
-const playground = select('.playground')
-  .selectAll('p')
-  .data(data)
-  .enter()
-    .append('p')
-    .text((d: string, i: number) => `Hello: ${d}, ${i}`);
+const vis = new Chart('.visual-container', [-50,50], []);
 
-const lines: ILine[] = [
-  {
-    id: 1,
-    x1: 5,
-    y1: 5,
-    x2: 20,
-    y2: 20,
-    stroke: 'red',
-    strokeWidth: 4
-  },
-  {
-    id: 2,
-    x1: 50,
-    y1: 50,
-    x2: 200,
-    y2: 200,
-    stroke: 'red',
-    strokeWidth: 4
-  }
-];
+select('#reset')
+  .on('click', () => {
+    vis.reset();
+  });
 
-svg
-  .selectAll('line')
-  .data(lines, (d: any) => d.id)
-  .enter()
-    .append('line')
-    .attr('x1', (d: ILine) => d.x1)
-    .attr('y1', (d: ILine) => d.y1)
-    .attr('x2', (d: ILine) => d.x2)
-    .attr('y2', (d: ILine) => d.y2)
-    .attr('stroke', 'blue')
-    .attr('stroke-width', 2)
-    .classed('division', true)
-    ;
+select("button.mode")
+  .on('click', () => {
+    vis.toggleMode();
+  });
